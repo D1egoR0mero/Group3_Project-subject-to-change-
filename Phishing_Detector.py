a81173cd9b10261107cacb4e8571f14e572100d2
diegor456 <diegor456@vt.edu>
2026-09-16 20:12:02 -0400

    python project ready for coding

--- Phishing_Detector.py
+++ Phishing_Detector.py
@@ -4,6 +4,27 @@ def main():
     print("Hi")
 
 
+def gui():
+    # setup the gui for the user
+    print("filler")
+
+def readFromGUI():
+    # Take the user url and give it to the program so it can parse
+    print("filler")
+
+
+def parse():
+    # Take the given URL as a String and check for signs of phishing
+    print("filler")
+
+def analyze():
+    # calculate the liklihood of a url being phishing and return it as a percentage.
+    print("filler")
+
+def showUser():
+    # Show the user the chances of the url being phishing, as a final result
+    print("filler")
+
 
 if __name__ == "__main__":
     main()
